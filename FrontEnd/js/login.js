20aa9be96a32f54abd63fa1a793620bb22539047
shinra <chokdingo@hotmail.com>
2023-04-01 12:20:11 +0200

    connexion v3

--- FrontEnd/js/login.js
+++ FrontEnd/js/login.js
@@ -2,39 +2,36 @@ let inputEmail = document.querySelector("#email");
 let inputPassword = document.querySelector("#password");
 let buttonSubmit = document.querySelector("#submit");
 
-    let email = inputEmail;
-    let password = inputPassword;
-    
-    
+let email = inputEmail;
+let password = inputPassword;
 
-    var OPTIONS = {
-        method: 'POST',
-
-        body: JSON.stringify({
-            "email": email,
-            "password": password
-          }),
-
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        
-        mode: 'cors',
-
-        cache: 'default'
-    };    
+var OPTIONS = {
+    method: 'POST',
+    body: JSON.stringify({
+        email: email,
+        password: password
+        }),
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    }, 
+    mode: 'cors',
+    cache: 'default'
+};    
       
 
 function conexionUser(){ 
-    fetch('http://localhost:5678/api/users/login', OPTIONS)
+    fetch('http://localhost:5678/api/users/login', console.table(OPTIONS), OPTIONS)
     .then((reponse) => reponse.json())
+    .then((data)=>{
+        console.log(data)
+    })
 }
 
-buttonSubmit.addEventListener("click", function (conexion) {
-    conexionUser(); 
+buttonSubmit.addEventListener("click", function (conexionUser) {
+    conexionUser()
 });
       
-
+ 
 
  
